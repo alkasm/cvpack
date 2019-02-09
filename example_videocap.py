27e1eec05261b5f4be1797c35be12b67e5cd77ff
alkasm <ar@reynoldsalexander.com>
2019-02-08 20:46:24 -0800

    Accidentally saved some trials..

--- example_videocap.py
+++ example_videocap.py
@@ -2,7 +2,5 @@ import cv2
 from cvtools import VideoCapture, VideoPlayer
 
 with VideoCapture(0) as cap:
-    print(help(cap))
-    quit()
     player = VideoPlayer(cap)
     player.play(loop=True, framefunc=lambda f: cv2.flip(f, 1))
