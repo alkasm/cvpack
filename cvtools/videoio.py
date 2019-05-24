8471c9438952228daab69eeb7ca3aa73254efd9b
Alexander Reynolds <ar@reynoldsalexander.com>
2019-05-24 14:37:30 -0700

    Add video writer

--- cvtools/videoio.py
+++ cvtools/videoio.py
@@ -44,34 +44,31 @@ class VideoCaptureProperty:
         self.__doc__ = self._docstring.format(p=VIDEO_CAPTURE_PROPS[name])
 
     def __get__(self, obj, objtype=None):
-        return obj.get(self.prop)
+        return obj.cap.get(self.prop)
 
     def __set__(self, obj, value):
-        if not obj.set(self.prop, value):
+        if not obj.cap.set(self.prop, value):
             raise AttributeError(self._set_err.format(p=VIDEO_CAPTURE_PROPS[self.name]))
 
 
 @add_props(VIDEO_CAPTURE_PROPS.keys())
-class VideoCapture(cv2.VideoCapture):
+class VideoCapture:
     """An adapter for `cv2.VideoCapture`, giving a more Pythonic interface."""
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if not self.isOpened():
+        self.cap = cv2.VideoCapture(*args, **kwargs)
+        if not self.cap.isOpened():
             raise ValueError("Unable to open video source:", *args, **kwargs)
+
+    def __getattr__(self, key):
+        return getattr(self.cap, key)
 
     def __iter__(self):
         """Resets the frame position to 0 at the start for repeatable iteration."""
-        try:
-            self.pos_frames = 0
-        except AttributeError:
-            pass
-        while self.isOpened():
-            success, frame = self.read()
-            if success:
+        noread = (False, None)
+        if self.cap.isOpened():
+            for _, frame in iter(self.cap.read, noread):
                 yield frame
-            else:
-                return
 
     def __enter__(self):
         """Enter the context manager."""
@@ -79,7 +76,55 @@ class VideoCapture(cv2.VideoCapture):
 
     def __exit__(self, exctype, exc, exctrace):
         """Releases the video capture object on exiting the context manager."""
-        self.release()
+        self.cap.release()
+
+
+class VideoWriter:
+
+    _nowriter = object()
+
+    def __init__(self, filename, fourcc="AVI1", fps=30, frameSize=None, **kwargs):
+        self.filename = str(filename)
+        self.fourcc = (
+            fourcc if isinstance(fourcc, int) else cv2.VideoWriter_fourcc(*fourcc)
+        )
+        self.fps = fps
+        self.kwargs = kwargs
+
+        # wait to create writer based on first frame if size is not provided
+        self.writer = (
+            self._nowriter if frameSize is None else self._makewriter(frameSize)
+        )
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        try:
+            self.writer.release()
+        except AttributeError as e:
+            if self.writer is not self._nowriter:
+                raise e
+
+    def _makewriter(self, frame_size):
+        return cv2.VideoWriter(
+            filename=self.filename,
+            fourcc=self.fourcc,
+            fps=self.fps,
+            frameSize=frame_size,
+            **self.kwargs
+        )
+
+    def write(self, frame):
+        try:
+            return self.writer.write(frame)
+        except AttributeError as e:
+            if self.writer is self._nowriter:
+                h, w = frame.shape[:2]
+                self.writer = self._makewriter((w, h))
+                self.write(frame)
+            else:
+                raise e
 
 
 class VideoPlayer:
