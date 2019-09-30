280fe6ace4527e2274766eeb61ea06bb51ad7817
Alexander Reynolds <ar@reynoldsalexander.com>
2019-09-29 23:42:05 -0700

    Update setup.py
    Do not require PyPI opencv.

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ setup(
     author="Alexander Reynolds <alkasm>",
     author_email="ar@reynoldsalexander.com",
     url="https://github.com/alkasm/cvtools",
-    version="0.4.1",
+    version="0.4.2",
     packages=find_packages(),
-    install_requires=["opencv-python"],
     license="MIT",
     long_description=open("README.md").read(),
 )
